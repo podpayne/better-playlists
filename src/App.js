e7a16f077d95d6d54865d04c8eee3df0c17442ce
Časlav Ljubičić <tasa1993@gmail.com>
2018-04-26 15:05:39 +0200

    Filter Component addition

--- src/App.js
+++ src/App.js
@@ -72,10 +72,12 @@ class HoursCounter extends Component{
 
 class Filter extends Component{
   render(){
+
     return(
       <div style={{defaultStyle}}>
         <img/>
-        <input type='text'/>
+        <input type='text' onKeyUp={event => 
+          this.props.onTextChange(event.target.value)}/>
         
       </div>
     );
@@ -102,13 +104,18 @@ class Playlist extends Component{
 class App extends Component {
   constructor(){
     super();
-    this.state = {serverData: {}}
+    this.state = {
+      serverData: {},
+      filterString: ''
+    }
   }
   componentDidMount(){
     setTimeout( ()=>{
       this.setState({serverData: fakeServerData});
     },1000);
+
   }
+
   render() {
     return (
       <div className="App" >
@@ -121,8 +128,13 @@ class App extends Component {
             this.state.serverData.user.playlists}/>
           <HoursCounter playlists={
             this.state.serverData.user.playlists}/>
-          <Filter/>
-          {this.state.serverData.user.playlists.map(playlist => 
+          <Filter onTextChange = {text =>
+            this.setState({filterString:text})
+          }/>
+          {this.state.serverData.user.playlists.filter(playlist =>
+            playlist.name.toLowerCase().includes(
+              this.state.filterString.toLowerCase())
+          ).map(playlist => 
             <Playlist playlist={playlist}/>
           )}
           
